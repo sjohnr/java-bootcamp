4759453e32e54e13552e2b8db332c35e244e9bb0
Stephen Riesenberg <sjohnr@hotmail.com>
2016-02-10 20:19:15 -0600

    StopWatch checkpoint

---
+++ src/main/java/com/java/bootcamp/util/StopWatch.java
@@ -0,0 +1,23 @@
+package com.java.bootcamp.util;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class StopWatch {
+    private AtomicBoolean active;
+
+    public StopWatch() {
+        this.active = new AtomicBoolean(false);
+    }
+
+    public void start() {
+        
+    }
+
+    public void stop() {
+        
+    }
+    
+    public void reset() {
+        
+    }
+}
